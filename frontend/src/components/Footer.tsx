324f484aa9f32deaf0643d3319f2698e58cac10c
anonrst <mdayan1qaz@gmail.com>
2025-10-14 04:51:12 +0000

    frontend added

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bottom-0 h-30 w-full bg-amber-800 mt-20 text-center'>
+        aaa
+    </div>
+  )
+}
+
+export default Footer
