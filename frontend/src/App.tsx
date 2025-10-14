20aabce09d19277669bf348a3a9279aa5b12cace
anonrst <mdayan1qaz@gmail.com>
2025-10-14 05:14:00 +0000

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Body from './components/body'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+      <div>
+        <Navbar/>
+        <Body/>
+      </div>
+
+    )
+}
+
+export default App
---
+++ frontend/src/types/ethereum.d.ts
@@ -0,0 +1,10 @@
+interface Window {
+  ethereum?: {
+    request: (args: { method: string; params?: any[] }) => Promise<any>;
+    on: (event: string, handler: (accounts: string[]) => void) => void;
+    removeListener: (
+      event: string,
+      handler: (accounts: string[]) => void
+    ) => void;
+  };
+}
